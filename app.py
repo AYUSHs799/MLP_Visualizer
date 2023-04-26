b02a7b373d1d745cf0c5ca1997e3990c82fb9c75
Ayush Shrivastava <ashri41997@gmail.com>
2023-04-26 11:49:48 +0530

    Final Changes

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def model_MLP(X_train,y_train,X_test,layers, nodes, activation, solver, rate, it
     model.compile(optimizer=opt,loss = 'mean_squared_error',metrics=['mean_squared_error'])
 
     # Fit model.
-    model.fit(X_train, y_train, epochs=iter, verbose=0)
+    model.fit(X_train, y_train, epochs=iter)
 
     # Evaluate model.
     y_hat = model.predict(X_test)
@@ -51,18 +51,38 @@ def model_MLP(X_train,y_train,X_test,layers, nodes, activation, solver, rate, it
 
 
 def get_model_summary(model):
+    """Gets the summary of the model"""
+
+    # Creating a stream to store the summary.
     stream = io.StringIO()
+
+    # Printing the summary to the stream.
     model.summary(print_fn=lambda x: stream.write(x + '\n'))
+
+    # Getting the summary from the stream.
     summary_string = stream.getvalue()
+
+    # Closing the stream.
     stream.close()
+
+    # Returning the summary.
     return summary_string
 
 
 
 if __name__ == '__main__':
 
-    # Adding a title to the app.
+    with st.sidebar:
+        # Adding a title to the app.
+        st.header('Multi Layer Perceptron Neural Network')
+
+        st.write('A multilayer perceptron(MLP) is a fully connected class of feedforward artificial neural network (ANN). The term MLP is used ambiguously, sometimes loosely to mean any feedforward ANN, sometimes strictly to refer to networks composed of multiple layers of perceptrons (with threshold activation)')
+
+        st.write('An MLP consists of at least three layers of nodes: an input layer, a hidden layer and an output layer. Except for the input nodes, each node is a neuron that uses a nonlinear activation function. MLP utilizes a chain rule based supervised learning technique called backpropagation or reverse mode of automatic differentiation for training.Its multiple layers and non-linear activation distinguish MLP from a linear perceptron. It can distinguish data that is not linearly separable.')
+
+
     st.title("Visualize MLPs")
+    st.write('This app is created to visualize the Multi layer perceptron Neural Network model. You can change the parameters and see the changes in the model architecture and the predictions.')
 
     # Adding a subtitle to the app.  
     st.subheader('MLP Parameters')
@@ -73,22 +93,22 @@ if __name__ == '__main__':
     with left_column:
 
         # slider for max iterations.
-        iter = st.slider('Max Iteration', min_value=100,max_value= 1000,value=500,step=10)
+        iter = st.slider('No. of Iterations to run', min_value=10,max_value= 1000,value=50,step=10)
         # slider for nodes per layer.          
-        nodes = st.slider('Nodes', min_value=1,max_value= 10,value=5,step=1) 
+        nodes = st.slider('No. of Nodes in each layer', min_value=1,max_value= 15,value=5,step=1) 
         # slider for number of hidden layers.                       
-        layers = st.slider('Hidden Layers', min_value=1,max_value= 10,value=3,step=1) 
+        layers = st.slider('No. of Hidden Layers in the Network', min_value=1,max_value= 10,value=3,step=1) 
         # selectbox for activation function.              
         activation = st.selectbox('Activation (Output layer will always be linear)',('linear','relu','sigmoid','tanh'),index=2)                  
         
     with right_column:
 
         # slider for adding noise.
-        noise = st.slider('Noise', min_value=0,max_value= 100,value=20,step=10)
+        noise = st.slider('Noise (Sinusoidal Noise)', min_value=0,max_value= 100,value=20,step=10)
         # slider for test-train split.                     
         split = st.slider('Test-Train Split', min_value=0.1,max_value= 0.9,value=0.3,step=0.1) 
         # selectbox for solver/optimizer.     
-        solver = st.selectbox('Solver',('adam','sgd'),index=0)           
+        solver = st.selectbox('Solver/Optimizer',('adam','sgd'),index=0)           
         # selectbox for learning rate.                           
         rate = float(st.selectbox('Learning Rate',('0.001','0.003','0.01','0.03','0.1','0.3','1.0'),index=3))   
 
@@ -97,7 +117,7 @@ if __name__ == '__main__':
     y = X + np.sin(X)*X/5*noise/50*np.random.choice([0,0.5,1,1.5]) + np.random.normal(0,2,250)*noise/100
 
     # Split data into training and test sets.    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split,random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split)
 
     # Predicting the test data.
     y_hat,model = model_MLP(X_train,y_train,X_test,layers, nodes, activation, solver, rate, iter)
@@ -112,7 +132,8 @@ if __name__ == '__main__':
 
         # Adding a subheader to the container.
         st.subheader('Predictions')
-
+        st.write('True function : y = x ')
+        st.write('Noise : some sinusoial function with random noise')
         # Adding two columns to display the graphs.
         left_graph, right_graph = st.columns(2)
 
@@ -122,7 +143,7 @@ if __name__ == '__main__':
             st.write('Training Data set')
 
             fig1, ax1 = plt.subplots(1)
-            ax1.scatter(X_train, y_train, label='train',color='blue',alpha=0.6,edgecolors='black')
+            ax1.scatter(X_train, y_train, label='train',color='blue',alpha=0.4,edgecolors='black')
 
             # setting the labels and title of the graph.
             ax1.set_xlabel('X')
@@ -140,7 +161,7 @@ if __name__ == '__main__':
             st.write('Test Data set')
 
             fig2, ax2 = plt.subplots(1)
-            ax2.scatter(X_test, y_test, label='test',color='blue',alpha=0.6,edgecolors='black')
+            ax2.scatter(X_test, y_test, label='test',color='blue',alpha=0.4,edgecolors='black')
 
             test = np.c_[(X_test,y_hat)]
             test = test[test[:,0].argsort()]
